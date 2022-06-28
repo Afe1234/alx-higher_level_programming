2bfcd74dc1983784fc301cd0760693c393ea6423
Afework <afe.derb1234@gmail.com>
2022-06-28 13:47:12 -0700

    Return num fixed

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        number = number * -1
-    num = number % 10
-    print("{}".format(num), end='')
-    return num 
+    last = number % 10 if number > 0 else number * -1 % 10
+    print('{:d}'.format(last), end="")
+    return(last) 
